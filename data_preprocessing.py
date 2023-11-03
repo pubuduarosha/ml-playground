787a8ea7216b775d9608b5c63542d016d7c45697
pubuduarosha <aroshapubudu@gmail.com>
2023-11-03 14:45:51 +0530

    Encoding Categorical Data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,20 +8,22 @@ X = data.iloc[:,:-1].values
 
 Y = data.iloc[:,3].values
 
-from sklearn.preprocessing import Imputer
-
-imputerVariable = Imputer(missing_values = 'NaN',
-                          strategy = 'mean',
-                          axis = 0)
-
-imputerVariable.fit(X[:,1:3])
-
-X[:, 1:3] = imputerVariable.transform(X[:, 1:3])
-
 from sklearn.impute import SimpleImputer
 
 # Create an instance of SimpleImputer with the 'mean' strategy for imputation
 imputerVariable = SimpleImputer(strategy='mean')
 
 # Fit the imputer on the data and transform the specified columns (1 and 2)
-X[:, 1:3] = imputerVariable.fit_transform(X[:, 1:3])
+X[:, 1:3] = imputerVariable.fit_transform(X[:, 1:3])    
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+# Create a LabelEncoder for the first column
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+# Create a OneHotEncoder and specify the column to one-hot encode
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = ct.fit_transform(X)
+
